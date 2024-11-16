5f07b2d9d2dcaaf396f7b380ba607a044bb3f1b5
Muhammad Zaidan Dhiyaulhaq <zaidan5dhiyaulhaq@gmail.com>
2024-11-16 13:40:15 +0700

    Fourth Commit

--- src/base/FrameReviewGame.java
+++ src/base/FrameReviewGame.java
@@ -16,7 +16,7 @@ public class FrameReviewGame extends javax.swing.JFrame {
     public FrameReviewGame() {
         initComponents();
     }
-
+// zayteluuu
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
