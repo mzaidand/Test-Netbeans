e4f4adfb490e141fbb5ab39769f8de81f21b1023
Muhammad Zaidan Dhiyaulhaq <zaidan5dhiyaulhaq@gmail.com>
2024-11-16 13:17:16 +0700

    Third Commit

--- src/base/menambahdatagame.java
+++ src/base/menambahdatagame.java
@@ -22,7 +22,7 @@ public class menambahdatagame extends javax.swing.JFrame {
     public menambahdatagame() {
         initComponents();
     }
-
+// testi commit
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
